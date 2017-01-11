bf64ca8f027add235e85242b773e1fd81d75836c
munozyanez <munoz.yanez@gmail.com>
2017-01-11 22:04:45 +0100

    modified:   app/oscillator.cpp

--- app/oscillator.cpp
+++ app/oscillator.cpp
@@ -12,6 +12,22 @@ int main()
 
 
     //Middleware
+
+    std::cout << "[Initial]";
+    //MWI::Port imuPort;
+    //INITIALISE AND CHECK YARP
+    yarp::os::Network yarpNet;
+
+    if ( !yarpNet.checkNetwork(2) )
+    {
+        std::cout << "[error] %s found no YARP network (try running \"yarp detect --write\")." << std::endl;
+        return -1;
+    }
+    else
+    {
+        std::cout << "[success] YARP network found." << std::endl;
+    }
+
     //Robot teo right arm
     std::stringstream robConfig;
     //YARP device
@@ -23,34 +39,56 @@ int main()
     MWI::Robot rightArm(robConfig);
 
 
-     int axis = 3;
+     int axis = 0;
     //Amplitude in degrees
     double A = 10;
     double acc = 0.1; //accurracy to reach amplitude
     //Period in secods
     double T = 10;
     //Number of repetitions
-    int loops = 2;
+    int loops = 4;
     double w = 2*M_PI/T;
     double waitYarp = 0.01;
 
     double actualPos=0;
     double actualVel=0;
     int direction=+1; // +1 forward, -1 backward
+    int edge=0; //when position is A, and vel = 0.
+
+    rightArm.SetJointPos(axis,0);
+    yarp::os::Time::delay(2);
 
     for (int i=0; i<loops; i++)
     {
-        while ( actualPos+acc < A )
+        while ( edge==0 )
         {
 
             actualPos=rightArm.GetJoint(axis);
             actualVel=direction*w*sqrt(A*A-actualPos*actualPos);
             rightArm.SetJointVel(axis,actualVel);
+            if (direction*actualPos-A > acc)
+            {
+                edge=1;
+            }
             yarp::os::Time::delay(waitYarp);
+            std::cout << T << ", v "
+                         << actualVel << ", p "
+                         << actualPos << ", edge "
+                         << edge << ","
+                         << std::endl;
 
         }
+
+        edge=0;
         direction=direction*-1;
+        std::cout << ", i "
+                     << i << ", dir "
+                     << direction << ","
+                     << "" << ","
+                     << std::endl;
     }
+
+
     rightArm.SetJointVel(axis,0);
 
 
