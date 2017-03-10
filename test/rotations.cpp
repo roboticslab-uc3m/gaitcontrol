63619a01053e38eb6630bca8c622629c884b1c2b
munozyanez <munoz.yanez@gmail.com>
2017-03-10 20:40:02 +0100

    modified:   lib/gait 	modified:   test/rotations.cpp

--- test/rotations.cpp
+++ test/rotations.cpp
@@ -13,18 +13,23 @@ using namespace teo;
 int main()
 {
 
-    kin::Pose pose1(0,-0.1285,-0.85);
-    pose1.ChangeRotation(0,1,0,-M_PI/2);
-    pose1.ChangeRotation(1,0,0,M_PI);
-    std::cout << "rotation: " << pose1.GetUx() << "," << pose1.GetUy() << "," << pose1.GetUz() << "," << pose1.GetAngle() << std::endl;
-    pose1.ChangeRotation(0,0,1,M_PI);
-    std::cout << "rotation: " << pose1.GetUx() << "," << pose1.GetUy() << "," << pose1.GetUz() << "," << pose1.GetAngle() << std::endl;
-    pose1.ChangeRotation(0,1,0,-M_PI/2);
-    std::cout << "final rotation: " << pose1.GetUx() << "," << pose1.GetUy() << "," << pose1.GetUz() << "," << pose1.GetAngle() << std::endl;
+    std::vector<double> rot;
 
+    kin::Pose pose1(0,0,0);
+    pose1.ChangeRotation(0,1,0,-M_PI/2);
+    pose1.GetRotation(rot);
+    std::cout << "rotation (0,1,0,-M_PI/2) " << rot << std::endl;
+    pose1.ChangeRotation(1,0,0,M_PI);
+    std::cout << "rotation:(1,0,0,M_PI) " << pose1.GetUx() << "," << pose1.GetUy() << "," << pose1.GetUz() << "," << pose1.GetAngle() << std::endl;
+    pose1.ChangeRotation(0,0,1,M_PI);
+    std::cout << "rotation:(0,0,1,M_PI) " << pose1.GetUx() << "," << pose1.GetUy() << "," << pose1.GetUz() << "," << pose1.GetAngle() << std::endl;
+    pose1.ChangeRotation(0,1,0,-M_PI/2);
+    std::cout << "final rotation:(0,1,0,-M_PI/2) " << pose1.GetUx() << "," << pose1.GetUy() << "," << pose1.GetUz() << "," << pose1.GetAngle() << std::endl;
+    std::cout << " " << std::endl;
+
+    //several rotations in one axis
     kin::Pose pose2(0,0,0);
     pose2.ChangeRotation(1,0,0,M_PI);
-    std::vector<double> rot;
     pose2.GetRotation(rot);
     std::cout << "rotation " << rot << std::endl;
     pose2.ChangeRotation(1,0,0,M_PI/3);
@@ -39,6 +44,29 @@ int main()
     pose2.ChangeRotation(1,0,0,M_PI);
     pose2.GetRotation(rot);
     std::cout << "rotation " << rot << std::endl;
+    std::cout << " " << std::endl;
+
+
+    //Relative rotations
+    pose1.SetRotation(0,1,0,M_PI/2);
+    pose1.ChangeRotation(1,0,0,M_PI);
+    pose1.GetRotation(rot);
+    std::cout << "pose1 rotation " << rot << std::endl;
+    pose2=pose1;
+    pose2.ChangeRotation(0,0,1,-M_PI/20);
+    pose2.GetRotation(rot);
+    std::cout << "pose2 rotation " << rot << std::endl;
+    kin::Pose segment(pose1,pose2);
+    segment.GetRotation(rot);
+    std::cout << "Relative rotation " << rot << std::endl;
+
+//    kin::Pose segmentfrom0(pose1.Invert(),);
+//    pose3.GetRotation(rot);
+//    std::cout << "Relative rotation " << rot << std::endl;
+
+
+
+
     return 0;
 }
 
