0908b21d5cbf4b1abd881a76e3dc67a9623e3c73
munozyanez <munoz.yanez@gmail.com>
2017-03-13 13:32:15 +0100

    new file:   test/lipm.cpp

---
+++ test/lipm.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <fstream>
+
+#include "GaitSupportPoligon.h"
+#include "MiddlewareInterface.h"
+#include "SensorIntegration.h"
+
+using namespace std;
+
+using namespace teo;
+
+
+int main()
+{
+
+    return 0;
+}
