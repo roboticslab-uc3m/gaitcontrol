86e24bd0766ed14eadcf91a9c64164e14d541501
munozyanez <munoz.yanez@gmail.com>
2017-05-10 21:18:31 +0200

    modified:   CMakeLists.txt 	modified:   lib/i-kinematics 	new file:   test/kinematics.cpp

---
+++ test/kinematics.cpp
@@ -0,0 +1,35 @@
+
+#include "IKinematics.hpp"
+
+int main()
+{
+
+    IKinematics teokin;
+
+    std::vector<double> q1 {0,0,0,0,0,M_PI/2};
+    std::vector<double> pose(12);
+    std::vector<double> q2(6);
+
+    teokin.LeftLegFwdKin(q1, pose);
+
+    for (long i = 0; i< pose.size(); i++)
+    {
+        std::cout << pose[i] << ",";
+    }
+    std::cout << std::endl;
+
+    teokin.LeftLegFwdKin(pose,q2);
+    for (long i = 0; i< q2.size(); i++)
+    {
+        std::cout << q2[i] << ",";
+    }
+    std::cout << std::endl;
+
+    teokin.RightLegFwdKin(q1, pose);
+
+    for (long i = 0; i< pose.size(); i++)
+    {
+        std::cout << pose[i] << ",";
+    }
+    std::cout << std::endl;
+}
