1b16b4ec7f7826e9001063c3c7f32ce715ad71c6
munozyanez <munoz.yanez@gmail.com>
2017-05-11 17:46:43 +0200

    modified:   lib/i-kinematics 	modified:   test/kinematics.cpp

--- test/kinematics.cpp
+++ test/kinematics.cpp
@@ -10,26 +10,44 @@ int main()
     std::vector<double> pose(12);
     std::vector<double> q2(6);
 
+
     teokin.LeftLegFwdKin(q1, pose);
 
+    std::cout << "LeftLegFwdKin: ";
     for (long i = 0; i< pose.size(); i++)
     {
         std::cout << pose[i] << ",";
     }
     std::cout << std::endl;
 
+
     teokin.LeftLegInvKin(pose,q2);
+
+    std::cout << "LeftLegInvKin: ";
     for (long i = 0; i< q2.size(); i++)
     {
         std::cout << q2[i] << ",";
     }
     std::cout << std::endl;
 
+
     teokin.RightLegFwdKin(q1, pose);
 
+    std::cout << "RightLegFwdKin: ";
     for (long i = 0; i< pose.size(); i++)
     {
         std::cout << pose[i] << ",";
     }
     std::cout << std::endl;
+
+
+    teokin.RightLegInvKin(pose,q2);
+
+    std::cout << "RightLegInvKin: ";
+    for (long i = 0; i< q2.size(); i++)
+    {
+        std::cout << q2[i] << ",";
+    }
+    std::cout << std::endl;
+
 }
