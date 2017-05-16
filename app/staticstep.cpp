12d48be00db0570916ad604f9dabceb462afa63b
munozyanez <munoz.yanez@gmail.com>
2017-05-16 18:02:26 +0200

    modified:   app/staticstep.cpp 	modified:   lib/gait 	modified:   lib/i-kinematics

--- app/staticstep.cpp
+++ app/staticstep.cpp
@@ -17,19 +17,21 @@ int main()
     MWI::Robot teoRightLeg(ROBOT,"rightLeg"), teoLeftLeg(ROBOT,"leftLeg");
     IKinematics teokin;
 
-    kin::Pose initialRightFoot(0,-0.1285,-0.85);
-    initialRightFoot.ChangeRotation(0,1,0,-M_PI/2);
-    initialRightFoot.ChangeRotation(1,0,0,M_PI);
-    kin::Pose initialLeftFoot(0,+0.15,-0.85);
-    initialLeftFoot.ChangeRotation(0,1,0,-M_PI/2);
+    kin::Pose initialRightFoot(0,-0.1285,-0.845);
+
+    kin::Pose initialLeftFoot(0,+0.1285,-0.845);
+
     //every gait operation start with instantiation and initialization of a Gait child class
     teo::GaitSupportPoligon step(initialRightFoot,initialLeftFoot);
 
 
     tra::SpaceTrajectory traRightLeg, traLeftLeg;
 
+    step.SetHipParameters(0.08,0.02,0.1);
+    step.SetKickParameters(0.07,0.04);
     step.BeforeStep();
     step.AddStepForward(1);
+    step.AfterStep();
     step.GetTrajectories(traRightLeg, traLeftLeg);
 
 
@@ -38,22 +40,32 @@ int main()
     std::vector<double> angsRightLeg(6,0), angsLeftLeg(6,0);
     double dtLeftLeg, dtRightLeg;
 
-    for (int i = 0; i < traRightLeg.Size(); i++)
+    double dt= 0.1;
+    for (double t = 0.1; t < traRightLeg.GetTotalDuration(); t=dt+t)
     {
-        traLeftLeg.GetWaypoint(i, kinposeLeftLeg, dtLeftLeg);
-        kinposeLeftLeg.GetPose(poseLeftLeg);
+        traLeftLeg.GetSample(t,kinposeLeftLeg);
+        kinposeLeftLeg.GetPoseMatrix(poseLeftLeg);
 
-        traRightLeg.GetWaypoint(i, kinposeRightLeg, dtRightLeg);
-        kinposeRightLeg.GetPose(poseRightLeg);
+        traRightLeg.GetSample(t, kinposeRightLeg);
+        kinposeRightLeg.GetPoseMatrix(poseRightLeg);
 
         teokin.LeftLegInvKin(poseLeftLeg, angsLeftLeg);
         teokin.RightLegInvKin(poseRightLeg, angsRightLeg);
+
+        std::transform(angsLeftLeg.begin(), angsLeftLeg.end(), angsLeftLeg.begin(),
+                                     std::bind1st(std::multiplies<double>(), 180/M_PI));
+        std::transform(angsRightLeg.begin(), angsRightLeg.end(), angsRightLeg.begin(),
+                                     std::bind1st(std::multiplies<double>(), 180/M_PI));
 
         teoLeftLeg.SetJointPositions(angsLeftLeg);
         teoRightLeg.SetJointPositions(angsRightLeg);
 
 
-        yarp::os::Time::delay(dtLeftLeg);
+        std::cout << "new waypoint: " << t << " will take " << dt << " seconds " << std::endl;
+        std::cout << "leftLeg" << angsLeftLeg << std::endl;
+        std::cout << "rightLeg" << angsRightLeg << std::endl;
+
+        yarp::os::Time::delay(dt);
 
     }
 
